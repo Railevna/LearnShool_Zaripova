5e68a5581966b62917ce9360576473596831c536
Student48 <Student48@48KAB-3>
2025-02-05 09:13:16 +0300

    Добавьте файлы проекта.

---
+++ LearnShool_Zaripova/Datebase/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnShool_Zaripova.Datebase
+{
+    internal class DbConnection
+    {
+        public static LearnShool_ZaripovaEntities learnShoolDB = new LearnShool_ZaripovaEntities();
+
+    }
+}
---
+++ LearnShool_Zaripova/PageS/ServisPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnShool_Zaripova.PageS
+{
+    /// <summary>
+    /// Логика взаимодействия для ServisPage.xaml
+    /// </summary>
+    public partial class ServisPage : Page
+    {
+        public ServisPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
